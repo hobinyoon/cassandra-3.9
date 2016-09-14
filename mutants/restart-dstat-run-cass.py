0e2860b531704d7b8663d39bbea6bc4d22e7f071
Hobin Yoon <hobinyoon@gmail.com>
2016-09-14 17:01:48 +0000

    reganize dstat log

--- mutants/restart-dstat-run-cass.py
+++ mutants/restart-dstat-run-cass.py
@@ -8,9 +8,11 @@ import sys
 import types
 
 sys.path.insert(0, "%s/work/mutants/ec2-tools/lib/util" % os.path.expanduser("~"))
-
 import Cons
 import Util
+
+sys.path.insert(0, "%s/work/mutants/ec2-tools/lib" % os.path.expanduser("~"))
+import BotoClient
 
 
 def main(argv):
@@ -25,6 +27,50 @@ def main(argv):
 	Util.RunSubp("cgexec -g memory:small_mem %s/work/mutants/cassandra/bin/cassandra -f" \
 			" | \grep --color=always --line-buffered -E '(^|^WARN)'" \
 			% os.path.expanduser("~"))
+
+
+_tags = None
+def GetEc2Tags():
+	global _tags
+	if _tags is not None:
+		return _tags
+
+	r = BotoClient.Get(GetRegion()).describe_tags()
+	#Cons.P(pprint.pformat(r, indent=2, width=100))
+	_tags = {}
+	for r0 in r["Tags"]:
+		res_id = r0["ResourceId"]
+		if InstId() != res_id:
+			continue
+		_tags[r0["Key"]] = r0["Value"]
+	return _tags
+
+
+_region = None
+def GetRegion():
+	global _region
+	if _region is not None:
+		return _region
+
+	# http://stackoverflow.com/questions/4249488/find-region-from-within-ec2-instance
+	doc       = Util.RunSubp("curl -s http://169.254.169.254/latest/dynamic/instance-identity/document", print_cmd = False, print_output = False)
+	for line in doc.split("\n"):
+		# "region" : "us-west-1"
+		tokens = filter(None, re.split(":| |,|\"", line))
+		#Cons.P(tokens)
+		if len(tokens) == 2 and tokens[0] == "region":
+			_region = tokens[1]
+			return _region
+
+
+_inst_id = None
+def InstId():
+	global _inst_id
+	if _inst_id is not None:
+		return _inst_id
+
+	_inst_id  = Util.RunSubp("curl -s http://169.254.169.254/latest/meta-data/instance-id", print_cmd = False, print_output = False)
+	return _inst_id
 
 
 def RestartDstat():
@@ -64,8 +110,14 @@ def RestartDstat():
 					time.sleep(0.1)
 
 		# Run dstat as a daemon
-		fn_out = "%s/work/mutants/log-volatile/dstat/%s.csv" \
-				% (os.path.expanduser("~"), datetime.datetime.now().strftime("%y%m%d-%H%M%S"))
+		dn = "%s/work/mutants/log/%s/%s/dstat" \
+				% (os.path.expanduser("~")
+						, GetEc2Tags()["job_id"]
+						, GetEc2Tags()["name"].replace("server", "s").replace("client", "c"))
+		Util.MkDirs(dn)
+
+		fn_out = "%s/%s.csv" \
+				% (dn, datetime.datetime.now().strftime("%y%m%d-%H%M%S"))
 		cmd = "dstat -tcdn -C total -D xvda,xvdb,xvdd,xvde,xvdf -r --output %s" % fn_out
 		Util.RunDaemon(cmd)
 
